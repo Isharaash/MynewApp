63fcd7bad08bcb6c6e4d535ce129fe6132cb58da
W. W. Ishara Ashen Wegapitiya <isharaashen1234@gmail.com>
2024-04-03 14:13:14 +0530

    usercontroller modify

--- src/main/java/com/example/app/Controller/UserController.java
+++ src/main/java/com/example/app/Controller/UserController.java
@@ -7,13 +7,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @RestController
 @RequestMapping("/api/users")
-public class UserController {
-
+public class UserController implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry)  {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
+    }
     @Autowired
     private UserService userService;
 
